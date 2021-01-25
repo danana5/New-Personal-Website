115568148324dea684919ec11c40c4db3d13e3b4
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-25 22:11:43 +0000

    added in text and linked footer buttons to relevant sites

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <v-app>
     <v-img
     :cover="true"
-    aspect-ratio=2
+    aspect-ratio=3
+    gradient="to top left, rgba(190,220,241,.53), rgba(230,32,72,.6)"
     src="@/components/assets/background.jpg"
     >
       <router-view/>
@@ -19,13 +20,25 @@
           class="cyan darken-3 text-center mx-auto"
         >
           <v-card-text>
-            <v-icon size="25px" v-for="item in items"
-            :key="item"
-            :to="item.to"
-            outlined
-            class="mx-4 light-green--text text--accent-3">
-              {{ item.icon }}
-            </v-icon>
+            <v-row justify="center">
+              <v-btn
+              v-for="item in items"
+              :key="item"
+              :href="item.to"
+              outlined
+              target="_blank"
+              icon
+              class="mx-4 light-green--text text--accent-3"
+              >
+                <v-icon size="25px">
+                  {{ item.icon }}
+                </v-icon>
+              </v-btn>
+            
+            </v-row>
+            <v-row justify="center" class="mx-4 light-green--text text--accent-3">
+              {{ new Date().getFullYear() }}-Daniel Grace
+            </v-row>           
           </v-card-text>
         </v-card>
       </v-footer>
@@ -37,10 +50,11 @@ export default {
   name: 'App',
   data: () => ({
     items: [
-        {icon: 'mdi-facebook', to: "/"},
-        {icon: 'mdi-twitter', to: "/"},
-        {icon: 'mdi-linkedin', to: "/"},
-        {icon: 'mdi-instagram', to: "/"},
+        {icon: 'mdi-facebook', to: "https://www.facebook.com/dananananananananananananana/"},
+        {icon: 'mdi-twitter', to: "https://twitter.com/Danana_5"},
+        {icon: 'mdi-linkedin', to: "https://www.linkedin.com/in/daniel-grace-892963194/"},
+        {icon: 'mdi-instagram', to: "https://www.instagram.com/danana_5/"},
+        {icon: 'mdi-github', to: "https://github.com/danana5"}
       ],
   }),
 };
