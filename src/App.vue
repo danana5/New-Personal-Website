f34e0a30ca46cdb5b9b4fb2da58bfe1481a85a5f
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-25 20:29:38 +0000

    added background image and router view

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,49 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+    <v-img
+    :cover="true"
+    aspect-ratio=2
+    src="@/components/assets/background.jpg"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <router-view/>
+    </v-img>
+    <v-footer
+        padless
+        width="100%"
+        app
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+        <v-card
+          flat
+          tile
+          width="100%"
+          class="cyan darken-3 text-center mx-auto"
+        >
+          <v-card-text>
+            <v-icon size="25px" v-for="item in items"
+            :key="item"
+            :to="item.to"
+            outlined
+            class="mx-4 light-green--text text--accent-3">
+              {{ item.icon }}
+            </v-icon>
+          </v-card-text>
+        </v-card>
+      </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    items: [
+        {icon: 'mdi-facebook', to: "/"},
+        {icon: 'mdi-twitter', to: "/"},
+        {icon: 'mdi-linkedin', to: "/"},
+        {icon: 'mdi-instagram', to: "/"},
+      ],
   }),
 };
 </script>
+<style>
+</style>
