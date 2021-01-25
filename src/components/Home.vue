eb7d9cf717438375cf262a8911827266be57f38b
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-25 20:30:23 +0000

    created home and created card

---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+ <v-container fill-height fill-width>
+    <v-sheet
+    class="mx-auto"
+    rounded
+    color="#D9BC6C"
+    >
+     <v-row
+      align="center"
+      justify="center"
+      class="pa-2"
+    >
+      <v-col class="text-center white--text" cols="12">
+        <v-avatar
+        size="150"
+        >
+        <v-img src="./assets/compressed.jpg"></v-img>
+        </v-avatar>
+        <h1 class="display-1 font-weight-thin mb-2 purple--text text--darken-1">Daniel Grace</h1>
+        <h4 class="subheading mb-4 white--text">Computer Science Student</h4>
+        <v-btn
+        class="mx-2"
+        outlined
+        rounded
+        color="purple darken-1"
+        >
+          About Me
+        </v-btn>
+        <v-btn
+        class="mx-2"
+        outlined
+        rounded
+        color="purple darken-1"
+        >
+          Projects
+        </v-btn>
+        <v-btn
+        class="mx-2"
+        outlined
+        rounded
+        color="purple darken-1"
+        >
+          Contact Me
+        </v-btn>
+        <v-btn
+        class="mx-2"
+        outlined
+        rounded
+        color="purple darken-1"
+        >
+          Old Website
+        </v-btn>
+      </v-col>
+    </v-row>
+   </v-sheet>
+ </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
