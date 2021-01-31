cd2a26e6f637f3063f4884ca2481d8771af1e48b
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-31 17:32:00 +0000

    reset projects page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,17 +1,13 @@
 <template>
-  <v-container fill-height>
-    <v-card
-    class="mx-auto"
-    outlined
-    >
-      <h5>Projects</h5>
-    </v-card>
-  </v-container>
+  <h5>Projects</h5>
 </template>
 
 <script>
 export default {
-
+  data () {
+      return {
+    }
+  }
 }
 </script>
 
