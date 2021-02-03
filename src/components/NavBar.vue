8e0a7932e5feed8120a6ad1b9fe78915a3395461
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-03 16:14:50 +0000

    created navbar component for compartmentalisation

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar 
+      toggleable="lg"
+      type="dark"
+      class="nav"
+      variant="faded"
+      sticky
+      >
+        <b-navbar-brand to="/">Daniel Grace</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#About">About Me</b-nav-item>
+            <b-nav-item href="#Projects">Projects</b-nav-item>
+            <b-nav-item href="#Contact">Contact Me</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
