5fb3ed3d87cca390b713b41d2b576466830afb9d
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-03 16:15:26 +0000

    added parallax hero image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,65 +1,40 @@
 <template>
-    <v-container fill-height>
-          <v-sheet
-            class="mx-auto"
-            tile
-            color="#a38c00"
-            max-width="600px"
+    <div>
+            <v-parallax
+                dark
+                src="../assets/background.jpg"
+                height= "750"
             >
+                <NavBar/>
                 <v-row
                 align="center"
                 justify="center"
-                class="pa-2"
                 >
-                    <v-col class="text-center white--text" cols="12">
-                        <v-avatar
-                        size="200"
-                        >
-                        <v-img src="../assets/compressed.jpg"></v-img>
-                        </v-avatar>
-                        <h1 class="display-1 font-weight-thin">Daniel Grace</h1>
-                        <h4 class="subheading purple--text text--lighten-2">Computer Science Student</h4>
-                        <v-btn
-                        class="mx-2"
-                        outlined
-                        rounded
-                        color="white"
-                        >
-                        About Me
-                        </v-btn>
-                        <v-btn
-                        class="mx-2"
-                        outlined
-                        rounded
-                        color="white"
-                        >
-                        Projects
-                        </v-btn>
-                        <v-btn
-                        class="mx-2"
-                        outlined
-                        rounded
-                        color="white"
-                        >
-                        Contact Me
-                        </v-btn>
-                        <v-btn
-                        class="mx-2"
-                        outlined
-                        rounded
-                        href="https://danana5.github.io/"
-                        color="white"
-                        >
-                        Old Website
-                        </v-btn>
-                    </v-col>
+                <v-col class="text-center" cols="12">
+                    <h1 class="display-1 font-weight-thin mb-4">Daniel Grace</h1>
+                    <h4 class="subheading">Computer Science Student</h4>
+                </v-col>
                 </v-row>
-        </v-sheet>
-    </v-container>
+            </v-parallax>
+        <v-flex id="About">
+            <About/>
+        </v-flex>
+        <v-flex>
+            <Projects/>
+        </v-flex>
+    </div>
+
 </template>
 
 <script>
+import NavBar from "./NavBar"
+import About from "./About"
+import Projects from "./Projects"
 export default {
-
+    components:{
+        About,
+        NavBar,
+        Projects
+    }
 }
 </script>
