0486be42a8d41109814397345246d09bbc6a791f
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-03 16:14:30 +0000

    removed router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/components/Home";
-import About from "@/components/About";
-import Projects from "@/components/Projects";
-import Contact from "@/components/Contact";
 
 Vue.use(VueRouter);
 
@@ -12,21 +9,6 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-  {
-    path: "/projects",
-    name: "Projects",
-    component: Projects,
-  },
-  {
-    path: "/contact",
-    name: "Contact",
-    component: Contact,
   },
 ];
 
