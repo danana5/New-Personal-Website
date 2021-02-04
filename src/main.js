94fcaf8a13b6ed53abe3fd057d8e7d98b9a916b0
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-04 00:11:51 +0000

    removed vue router dependency

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
